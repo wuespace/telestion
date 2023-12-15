4e74ed95368793c8cb0a22e401e025b2d621e232
Ludwig Richter <ludwig.richter@wuespace.de>
2023-12-15 14:39:28 +0100

    TELESTION-435: Release npm package

--- frontend-react/vite.config.ts
+++ frontend-react/vite.config.ts
@@ -1,9 +1,19 @@
 /// <reference types="vitest" />
-import { resolve } from 'path';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import banner from 'vite-plugin-banner';
 
 import packageJson from './package.json' assert { type: 'json' };
+
+const bannerText = {
+	name: packageJson.name,
+	version: packageJson.version,
+	description: packageJson.description,
+	author: `${packageJson.author.name} <${packageJson.author.email}>`,
+	homepage: packageJson.homepage,
+	license: 'Copyright (c) 2023 WüSpace e. V.'
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,8 +33,12 @@ export default defineConfig({
 			}
 		}
 	},
-	plugins: [react()],
+	plugins: [react(), banner(transformBanner(bannerText))],
 	test: {
 		environment: 'happy-dom'
 	}
 });
+
+function transformBanner(banner: Record<string, string>) {
+	return '/**\n' + Object.entries(banner).map(([key, value]) => ` * ${key}: ${value}\n`).join('') + '**/';
+}
