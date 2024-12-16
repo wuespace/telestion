4412420e0a4f5c2b42e491624d8bbc8acef21ff0
Ludwig Richter <ludwig.richter@wuespace.de>
2024-12-16 17:40:57 +0100

    source does not exist in posix :see_no_evil:

--- backend-go/test.sh
+++ backend-go/test.sh
@@ -3,5 +3,5 @@
 script_dir="$(dirname "$(realpath "$0")")"
 backend_features_dir="$script_dir/../backend-features"
 
-source "$backend_features_dir/.venv/bin/activate"
+. "$backend_features_dir/.venv/bin/activate"
 python3 "$backend_features_dir/run-tests.py" "$script_dir"
